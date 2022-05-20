c6d5b74cd54c07c41878b5f6c4388d41114a10aa
Joao P G Begiato <jpbegiato@hotmail.com>
2022-05-19 23:34:05 -0300

    Adiciona modelo organizacional MVC

---
+++ js/models/cadastro-models.js
@@ -0,0 +1,13 @@
+class User {
+    constructor(name, login, password, adress,phone, birthday){
+        this.name = name
+        this.login = login
+        this.password = password
+        this.adress = adress
+        this.phone = phone
+        this.birthday = birthday
+    }
+
+
+}
+
