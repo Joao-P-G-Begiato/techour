acaacad501a336f2472620ec1e19af729362bde7
Joao P G Begiato <jpbegiato@hotmail.com>
2022-05-22 20:05:36 -0300

    Altera funções de pasta MVC e adiciona estilização

--- js/controllers/cadastro-controllers.js
+++ js/controllers/cadastro-controllers.js
@@ -5,31 +5,11 @@ $("#cep-sub").click(function(e){
     manipulaCEP($("#cep").val())
 })
 
-function confereCEP (cep){
-    if(cep.length == 8){
-        chamaCEP(cep)
-        $(".invalido").empty()
-        $("#cep").attr("class" ,"form-control form-control-sm is-valid")
-    }else{
-        $(".invalido").html("CEP informado é inválido tente novamente")
-        $(".invalido").css("color", "red")
-        $("#cep").attr("class" ,"form-control form-control-sm is-invalid")
-    }
-}
 
 
 
-function manipulaCEP(){
-    const ref = "0123456789"
-    let entrada = $("#cep").val().split("")
-    let retorno =""
-    for(let i=0 ; i< entrada.length; i++){
-        if (ref.indexOf(entrada[i])>=0){
-            retorno += entrada[i]
-        }
-    }
-    return confereCEP(retorno)
-}
+
+
 
 
 function chamaCEP(cep){
