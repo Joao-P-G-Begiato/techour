262604c5adc4d1e886b8ad6f393f2fc39c959c3b
Joao P G Begiato <jpbegiato@hotmail.com>
2022-05-22 23:41:15 -0300

    adiciona Padronização do Header e Footer e Revisão de Identação

--- js/recupera.js
+++ js/recupera.js
@@ -1,20 +1,18 @@
+let botao = document.querySelector('#btn1');
 
-
- let botao = document.querySelector('#btn1');
- botao.addEventListener('click', function(evento){
-     evento.preventDefault();
-    
-    let entradaUsuario = document.querySelector('#formulario');
-    entradaUsuario.reset(); 
-    guardar();
+botao.addEventListener('click', function(evento){
+   evento.preventDefault(); 
+   let entradaUsuario = document.querySelector('#formulario');
+   entradaUsuario.reset(); 
+   guardar();
  })
 
- function guardar(){
-    let mensagem = document.querySelector('#mensagemvalidacao');
-    mensagem.innerHTML = 'Enviado com sucesso! Acesse o link para recuperação de senha enviado para o seu e-mail.'
+function guardar(){
+   let mensagem = document.querySelector('#mensagemvalidacao');
+   mensagem.innerHTML = 'Enviado com sucesso! Acesse o link para recuperação de senha enviado para o seu e-mail.'
   // return mensagem;
      
- }
+}
 
 
 //  $('#btn1').on('click', function(event){
--- js/views/cadastro-views.js
+++ js/views/cadastro-views.js
@@ -11,29 +11,32 @@ function logradouro(rua){
     $("#logradouro").removeAttr("disabled")
     $("#logradouro").val(rua)
 }
+
 function bairro(bairro){
     $("#bairro").removeAttr("disabled")
     $("#bairro").val(bairro)
+
 }
+
 function cidade(cidade){
     $("#cidade").removeAttr("disabled")
     $("#cidade").val(cidade)
 }
+
 function estado(estado){
     $("#estado").val(estado)
     $("#estado").removeAttr("disabled")
 }
+
 function pais(){
     $("#pais").val("Brasil")
     $("#pais").removeAttr("disabled")
 }
+
 function complemento(comp){
     $("#complemento").removeAttr("disabled")
     $("#complemento").val(comp)
 }
-
-
-
 
 function validaSenha(txt){
     if(pattern.test(txt) && pattern2.test(txt) && pattern3.test(txt) && pattern4.test(txt)){
